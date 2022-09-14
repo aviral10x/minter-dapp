40b2377f934e60c6af057ac9e3802be16fb6da81
Abrar Sanandwala <abrarsanandwala@gmail.com>
2022-09-14 22:58:46 +0530

    responsive

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -35,6 +35,18 @@ window.addEventListener("DOMContentLoaded", async() => {
         // height: "calc(100vh - 90px)",
         width: '100%',
         autoHeight: true,
+        breakpoints: {
+            640: {
+                perPage: 2,
+                gap: '.7rem',
+                height: '6rem',
+            },
+            480: {
+                perPage: 1,
+                gap: '.7rem',
+                height: '6rem',
+            },
+        },
     });
     splide.mount();
 
