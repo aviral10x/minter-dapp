d19610ad64ee378e0eb1c12dca9863574478e817
Daynerds <daynerds5555@gmail.com>
2022-11-06 20:18:50 +0530

    config.js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
 const contractAddress = "0xd571a0FcA0bee1165d6F0284246730996A3eaF09"; // Replace with your own contract address
-const chain = 'polygon'; // goerli, polygon, or ethereum
+const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "coming soon!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
