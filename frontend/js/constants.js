28cc497fc0fbd0567562174f70c8b0af30bd0152
AVIRAL GUPTA <91749530+aviral10x@users.noreply.github.com>
2023-10-18 21:35:42 +0530

    smooooooth

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,18 +1,22 @@
-const contractAddress = "0xd571a0FcA0bee1165d6F0284246730996A3eaF09"; // Replace with your own contract address
-const chain = 'polygon'; // goerli, polygon, or ethereum
+const contractAddress = "0x4F9824e93be85EAcF7a24005E87f70263ba4232e"; // Replace with your own contract address
+const chain = "goerli"; // goerli, polygon, or ethereum
 
 const welcome_h1 = "MINT IS Now live!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = "lets's go lets' go we may Airdrop 100 MATIC to one lucky minter after successful MINT. This NFT is just created for fun All holders will receive free airdrop from next NFT collection of Nerdsstay tuned for the updates";
+const welcome_p =
+  "lets's go lets' go we may Airdrop 100 MATIC to one lucky minter after successful MINT. This NFT is just created for fun All holders will receive free airdrop from next NFT collection of Nerdsstay tuned for the updates";
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Public mint will start soon";
 const h1_public_mint = "Public Minting Open!!";
 const h2_presale_coming_soon = "public mint countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
+const p_presale_coming_soon =
+  "We are working hard to launch the NFT Collection. Stay tuned for updates!";
 const p_presale_mint_not_whitelisted = "public sale will start soon ";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
+const p_presale_mint_whitelisted =
+  "You're on the whitelist for the pre-sale! 🎉";
+const p_presale_mint_already_minted =
+  "You've already claimed your whitelist mint. Thank you! 🎉";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 // const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_coming_soon = "Join the Discord";
